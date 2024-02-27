117fb49ec048a7eccb57243b60b5afe613ad32a0
Metehan Sözen <metehansozen@outlook.com>
2024-02-27 22:05:47 +0300

    admin tablosu migrate edildi, 404 sayfası eklendi, admin sayfası temelleri atıldı, category crud işlemleri yapıldı

--- TasteFoodIt/Context/TasteContext.cs
+++ TasteFoodIt/Context/TasteContext.cs
@@ -7,18 +7,17 @@ using TasteFoodIt.Entities;
 
 namespace TasteFoodIt.Context
 {
-	public class TasteContext : DbContext
-	{
-		public DbSet<About> Abouts { get; set; }
-		public DbSet<Adress> Adresses { get; set; }
-		public DbSet<Category> Categories { get; set; }
-		public DbSet<Chef> Chefs { get; set; }
-		public DbSet<Contact> Contacts { get; set; }
-		public DbSet<OpenDayHour> OpenDayHours { get; set; }
-		public DbSet<Product> Products { get; set; }
-		public DbSet<Reservation> Reservations { get; set; }
-		public DbSet<Testimonial> Testimonials { get; set; }
-	}
-
-
+    public class TasteContext : DbContext
+    {
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Adress> Adresses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Chef> Chefs { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<OpenDayHour> OpenDayHours { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+    }
 }
